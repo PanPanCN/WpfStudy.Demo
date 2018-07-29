50389504f5f78a993dedfccd04b211770169fd99
PanPan <hupan19931014@vip.qq.com>
2018-07-29 20:09:30 +0800

    通过CommandParameter传递对象

---
+++ TransmitObjectParameter.Demo/Converters/UserMultiValueConverter.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Windows.Data;
+using TransmitObjectParameter.Demo.Models;
+
+namespace TransmitObjectParameter.Demo.Converters
+{
+    public class UserMultiValueConverter : IMultiValueConverter
+    {
+        /// <inheritdoc />
+        /// <summary>
+        /// 对象转换
+        /// </summary>
+        /// <param name="values">所绑定的源的值</param>
+        /// <param name="targetType">目标的类型</param>
+        /// <param name="parameter">绑定时所传递的参数</param>
+        /// <param name="culture">系统语言等信息</param>
+        /// <returns></returns>
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (values.Cast<string>().Any(string.IsNullOrEmpty) && values.Length != 3)
+            {
+                return null;
+            }
+
+            var user = new UserModel()
+            {
+                Name = values[0].ToString(),
+                Phone = values[1].ToString(),
+                Email = values[2].ToString(),
+            };
+            return user;
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ TransmitObjectParameter.Demo/Models/UserModel.cs
@@ -0,0 +1,42 @@
+﻿using GalaSoft.MvvmLight;
+
+namespace TransmitObjectParameter.Demo.Models
+{
+    public class UserModel : ObservableObject
+    {
+        private string _name;
+        private string _email;
+        private string _phone;
+
+        public UserModel()
+        {
+
+        }
+        /// <summary>
+        /// 用户名
+        /// </summary>
+        public string Name
+        {
+            get => _name;
+            set => Set(() => Name, ref _name, value);
+        }
+
+        /// <summary>
+        /// 用户电话
+        /// </summary>
+        public string Phone
+        {
+            get => _phone;
+            set => Set(() => Phone, ref _phone, value);
+        }
+
+        /// <summary>
+        /// 用户邮件
+        /// </summary>
+        public string Email
+        {
+            get => _email;
+            set => Set(() => Email, ref _email, value);
+        }
+    }
+}
